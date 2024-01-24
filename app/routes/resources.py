5f29ab8384b5b480019091b2f7d846f33c4ac44e
Yonner-suarezLilab <yonner.suarez@lilab.pe>
2024-01-23 19:58:56 -0500

    change routes

--- app/routes/resources.py
+++ app/routes/resources.py
@@ -7,7 +7,7 @@ from app.Utils.PropuestaEmpleadores import PropuestaEmpleador
 from ..models.tbl_aichamba_empleador import tbl_aichamba_empleador
 from ..models.tbl_aichamba_empleado import tbl_iachamba_empleado
 from ..Utils.db import db 
-from ..api_models import new_Employee
+from ..api_models import new_Employee, new_employer
 
 Employee = Namespace("Employees")
 Employer = Namespace("Employer")
@@ -15,10 +15,29 @@ Employer = Namespace("Employer")
 @Employee.route("/GetEmployeesData")
 class Employees(Resource):
     def get(self):
+        # Obtén los datos del cuerpo de la solicitud utilizando el modelo new_Employee
+        data = request.json
+
+        # Accede a los campos del modelo
+        name = data.get('Name')
+        phone = data.get('Phone')
+
+        new_employee = tbl_iachamba_empleado(nombre=name, Telefono=phone)
+
+        # Agrega y guarda en la base de datos
+        db.session.add(new_employee)
+        db.session.commit()
+
+        return jsonify({"response": DataEmpleado})
+    
+@Employee.route("/addEmployee")
+class AddEmployees(Resource):
+    @Employer.expect(new_employer)
+    def post(self):
         return jsonify({"response": DataEmpleado})
     
 @Employee.route("/GetSummaryEmployee")
-class Employees(Resource):
+class EmployeesSumary(Resource):
     def get(self):
         return jsonify({"response": ResumenEmpleado})
 
@@ -28,7 +47,7 @@ class Employees(Resource):
         return jsonify({"response": DataEmpleador})
     
 @Employer.route("/GetProposalEmployer")
-class Employees(Resource):
+class EmployeesPropuesta(Resource):
     def get(self):
         return jsonify({"response": PropuestaEmpleador})
     
@@ -45,7 +64,7 @@ class Employees(Resource):
         
         # Crea una nueva instancia de EmployerDB
         new_employeer = tbl_aichamba_empleador(nombre=name, email=email)
-        new_employeer = tbl_iachamba_empleado()
+       
 
         # Agrega y guarda en la base de datos
         db.session.add(new_employeer)
